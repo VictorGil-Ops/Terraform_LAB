9f655884519adc8f0dc76adc5139b08f0fa55499
VictorGil-Ops <victorgilasp@gmail.com>
2022-06-02 22:44:20 +0200

    ec2

--- aws-ec2-instance/main.tf
+++ aws-ec2-instance/main.tf
@@ -1,26 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
   profile = var.profile
   region  = var.region
 }
 
 module "vpc" {
-  source = "../modules/vpc"
+  source = "./modules/vpc"
 }
 
 module "public_subnet" {
-  source = "../modules/public-subnet"
+  source = "./modules/public-subnet"
 
   vpc_id = module.vpc.vpc_id
 }
 
 module "internet_gateway" {
-  source = "../modules/internet-gateway"
+  source = "./modules/internet-gateway"
 
   vpc_id = module.vpc.vpc_id
 }
 
 module "route_table" {
-  source = "../modules/route-table"
+  source = "./modules/route-table"
 
   vpc_id              = module.vpc.vpc_id
   internet_gateway_id = module.internet_gateway.internet_gateway_id
@@ -28,7 +37,7 @@ module "route_table" {
 }
 
 module "ec2" {
-  source = "../modules/ec2"
+  source = "./modules/ec2"
 
   vpc_id                  = module.vpc.vpc_id
   public_subnet_id        = module.public_subnet.public_subnet_id
